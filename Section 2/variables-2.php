526fb109d30964aa242f7490efa07ba6a0515be5
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-22 12:32:54 +0000

    Echo and Print - how to view PHP output

---
+++ Section 2/variables-2.php
@@ -0,0 +1,21 @@
+<?php
+
+// Echo and Print - how to view PHP output
+
+$firstName = "Sipo";
+
+echo $firstName;
+
+// Line break
+echo "<br>";
+
+$a = 1;
+
+echo $a;
+
+echo "<br>";
+
+$b = TRUE;
+
+print $b;
+
