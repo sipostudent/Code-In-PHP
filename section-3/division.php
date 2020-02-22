04a07295eeeab17577fb4d3f52d62b545df0dd57
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-22 13:12:41 +0000

    Multiplication and Division

---
+++ section-3/division.php
@@ -0,0 +1,9 @@
+<?php
+
+$number1 = 100;
+
+$number2 = 25;
+
+$answer = $number1 / $number2;
+
+echo $answer;
---
+++ section-3/multiplication.php
@@ -0,0 +1,9 @@
+<?php
+
+$number1 = 4;
+
+$number2 = 8;
+
+$answer = $number1 * $number2;
+
+echo $answer;
