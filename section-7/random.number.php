54dd888d7faa7f368deb5fabbabddb49c70e0665
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-22 22:14:39 +0000

    How to generate a random number

---
+++ section-7/random.number.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+    Section 7: PHP Internal (in-built) Functions
+    ============================================
+*/
+
+/*  How to generate a random number
+    -------------------------------
+*/
+
+$randomNumber = rand(1, 100);
+
+echo $randomNumber;
