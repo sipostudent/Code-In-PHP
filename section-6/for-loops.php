0cd6e5cab651e46da31050ea879410647692cb7f
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-22 20:47:21 +0000

    How to use a FOR loop

--- section-6/for-loops.php
+++ section-6/for-loops.php
@@ -1,6 +1,23 @@
 <?php
 
-// How to use a FOR loop
+/*
+    Section 6: Basic Loops
+    ======================
+*/
+
+/*
+    How to use a FOR loop
+    ---------------------
+*/
+
+// Example 1
 for ($i = 1; $i <= 10; $i++){
+    echo $i . "<br>";
+}
+
+echo "<br>";
+
+// Example 2
+for ($i = 2; $i <= 20; $i+=2){
     echo $i . "<br>";
 }
