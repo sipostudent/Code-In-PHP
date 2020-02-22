d38034c7a1500ddb0097ba10564b0c0ac57ab182
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-22 15:40:57 +0000

    more concatenation

---
+++ section-4/concatenation2.php
@@ -0,0 +1,12 @@
+<?php
+
+$sentence = "The  quick brown fox jumped over the lazy dog but the dog was not at all happy about it and started to bark at the fox.";
+
+$sentence2 = "The quick brown fox ";
+$sentence2 .= "jumped over the lazy dog ";
+$sentence2 .= "but the dog was not at all happy ";
+$sentence2 .= "about it and started to bark at the fox.";
+
+echo $sentence;
+echo "<br>";
+echo $sentence2;
