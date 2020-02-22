2c93004510706fd0cb6deab9c00c5e8416314d8f
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-22 16:05:57 +0000

    Counting: Incrementing and Decrementing

---
+++ section-4/counting.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+    Assignment, Conditional and Logical Operators
+    =============================================
+*/
+
+/*
+    Counting: Incrementing and Decrementing
+    ---------------------------------------
+*/
+
+$i = 0;
+
+echo $i . "<br>";
+
+$i = $i - 1;
+
+echo $i . "<br>";
+
+$i -= 1;
+
+echo $i . "<br>";
+
+$i--;
+
+echo $i . "<br>";
