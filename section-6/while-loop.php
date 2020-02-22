50d73fc5b04c42ed50046139e5dd479bd5ad8ca7
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-22 20:53:29 +0000

    How to use a WHILE loop

---
+++ section-6/while-loop.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+    Section 6: Basic Loops
+    ======================
+*/
+
+/*
+    How to use a WHILE loop
+    -----------------------
+*/
+
+// Example 1
+$i = 1;
+
+while ($i <= 10) {
+    echo $i . "<br>";
+    $i++;
+}
+
+echo "<br>";
+
+// Example 1
+$i = 2;
+
+while ($i <= 20) {
+    echo $i . "<br>";
+    $i+=2;
+}
