d681b7f7be2a0e76cdab78505071f7d335d07dd5
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-22 15:14:47 +0000

    How to join words and sentences using concatenation

---
+++ section-4/concatenation.php
@@ -0,0 +1,24 @@
+<?php 
+
+// First String 
+$firstName = "Sipo"; 
+
+// Second String 
+$lastName = "Charles!"; 
+
+// Concatenation Of String 
+$concatenation = $firstName ."  ". $lastName; 
+
+// print Concatenate String 
+echo " $concatenation \n"; 
+
+echo "<br>";
+
+$length = 10;
+
+$width = 5;
+
+$rectangle = $length * $width;
+
+echo "The area of a rectangle is " .  $rectangle . " units squared.";
+?> 
