b12bdbd20cf78dedd622193e2e09ae3bf8bf35e8
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-23 17:44:29 +0000

    How to explode and implode strings

---
+++ section-10/explode-implode-arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+// How to explode and implode strings
+$string = "John, Sarah, Ken, Ben, Amy";
+
+$names = explode(", ", $string);
+
+echo "<pre>";
+print_r($names);
+
+$imploded = implode("|", $names);
+
+echo $imploded;
