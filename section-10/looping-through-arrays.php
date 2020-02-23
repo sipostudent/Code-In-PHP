5943d3959ab817d4b208c50262866c0e992efb74
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-23 16:28:15 +0000

    How to loop through an array in PHP?

--- section-10/looping-through-arrays.php
+++ section-10/looping-through-arrays.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+    How to create abd use basic ARRAYS and array functions
+    ======================================================
+*/
+
+/*
+    How to loop through an array in PHP?
+    ------------------------------------
+*/
+
 $names = array('Mary', 'James', "John", 'Sharon');
 
 echo "<ol>";
