cbf652a68ed55e1ff6a5452fcca7c5a10e25f4b0
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-23 16:12:04 +0000

    What is an array in PHP?

---
+++ section-10/array.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+    How to create abd use basic ARRAYS and array functions
+    ======================================================
+*/
+
+/*
+    What is an array in PHP?
+    ------------------------
+*/
+
+$names  = array('Mary', 'James', "John", 'Sharon');
+
+echo "<pre>";
+print_r($names);
+
+echo "<br>";
+
+$cities = ['London', 'Paris', 'New York'];
+
+print_r($cities);
+
+$mixed = array (5, 'red', "John", -3);
+
+print_r($mixed);
