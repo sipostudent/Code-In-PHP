c8ae3b63c3a7db72aaa7e19327b4d01f7aecdb6a
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-23 04:24:14 +0000

    How to escapte strings

---
+++ section-9/escaping-strings.php
@@ -0,0 +1,13 @@
+<?php
+
+// How to escapte strings
+
+/*
+
+Tom's got a great book called "Hello, World!"
+
+*/
+
+echo "Tom's got a great book called \"Hello, World!\"";
+
+echo 'Tom\'s got a great book called "Hello, World!"';
