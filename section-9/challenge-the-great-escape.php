cac1289445b9853447d463535d10a93f7601ba0a
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-23 04:30:56 +0000

    Challenge: The Great Escape - Escaping Strings

---
+++ section-9/challenge-the-great-escape.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+This is Sam's apple!
+
+Sam said, "Hello, Colin. How's your new PHP course going?".
+
+Sam's mother ordered a new book called "A Day in my Life" from her local bookstore.
+
+*/
+
+echo "This is Sam's apple!";
+
+echo "<br>";
+
+echo "Sam said, \"Hello, Colin. How's your new PHP course going?\".";
+
+echo "<br>";
+
+echo 'Sam\'s mother ordered a new book called "A Day in my Life" from her local bookstore.';
---
+++ section-9/lecture73extension.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = "Sam";
+
+echo "This is " . $name . "'s apple!";
+
+echo "<br>";
+
+echo $name . " said, \"Hello, Colin. How's your new PHP course going?\".";
+
+echo "<br>";
+
+echo $name . '\'s mother ordered a new book called "A Day in my Life" from her local bookstore.';
