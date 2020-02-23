66fd616072de9d4473075c2a00ad725a102109d3
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-23 00:05:40 +0000

    How to use the date function

---
+++ section-7/date.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+    Section 7: PHP Internal (in-built) Functions
+    ============================================
+*/
+
+/*  How to use the date function in PHP
+    -----------------------------------
+*/
+
+date_default_timezone_set('Europe/London');
+
+echo "The  year is " . date('Y');
+
+echo "<br>";
+
+echo "Today is " . date(l);
+
+echo "<br>";
+
+echo "The  time is " . date('g: i: a');
